96a36404035998cfc44e52b45b2a0c4b9675fb35
dudagasparotto <dudagasparotto05@gmail.com>
2026-02-27 22:46:28 -0300

    introducao

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import ReactDom from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './estilos/reset.css'
+import './estilos/globals.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
